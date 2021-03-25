78d69ae004c72dea0d596247eb0554157a486e5e
Chloe Chan <51076402+C-H-Y-I-N-G@users.noreply.github.com>
2021-03-25 01:33:11 -0700

    Add files via upload

---
+++ group_project_data.R
@@ -0,0 +1,43 @@
+############################################################
+# BUS 462 | Spring 2021 
+# Group Project 
+# Team Omega
+# I pledge on my honor that I have neither received nor given unauthorized assistance on this deliverable.
+############################################################
+
+#### PREAMBLE : ## Clearing buffers ####
+cat("\014")  # Clear Console
+rm(list = ls(all.names = TRUE))# clear all
+gc()
+
+# libraries
+
+require(data.table)
+require(stargazer)
+require(ggplot2)
+require(PerformanceAnalytics)
+
+#### LOAD DATA
+#laptimes <- fread("C:/Users/chloe/Desktop/BUS 462/group project/f1db_csv/lap_times.csv")
+#drivers <- fread("C:/Users/chloe/Desktop/BUS 462/group project/f1db_csv/drivers.csv")
+
+circuits <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/circuits.csv?token=ARGCUJVFLHKMHJWC6XIH7BDALPFH2")
+constructor_results <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_results.csv?token=ARGCUJVTEA7CUOKHQGVEMSTALPFUQ")
+constructor_standings <-fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_standings.csv?token=ARGCUJVL4UAEH6G4MG6FX5DALPGEE")
+constructors <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructors.csv?token=ARGCUJRM5BXALZ4LNR3HBZTALPGOW")
+driver_standings <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/driver_standings.csv?token=ARGCUJUEKGGVYUIJUPBIEF3ALPGRS")
+drivers <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/drivers.csv?token=ARGCUJVAAFN42GEWVT53LTDALPGU2")
+lap_times <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/lap_times.csv?token=ARGCUJRBHFWPLGAR6JIU65TALPGZ4")
+pit_stops <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/pit_stops.csv?token=ARGCUJSONNTEXU74NYPGU3DALPG7Q")
+qualifying <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/qualifying.csv?token=ARGCUJX6ZEK3OU273Z62EETALPHFA")
+races <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/races.csv?token=ARGCUJTJIFZSYPYKCUAZJ2TALPHJG")
+results <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/results.csv?token=ARGCUJVOFVMB7TWWVUIK3DDALPHNC")
+seasons <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/seasons.csv?token=ARGCUJRUCR5MDWODLISRETLALPHYI")
+status <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/status.csv?token=ARGCUJSK3IMT54XZMKATKG3ALPH4M")
+
+head(results)
+
+dt <- merge(lap_times,drivers,by="driverId") #merge 2 data frame
+dt <- subset(dt, select = -c(url)) #drop url column
+
+
