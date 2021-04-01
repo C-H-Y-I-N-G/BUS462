21a0dbf6301fadd082749f49258cdede3a18d09e
Chloe Chan <51076402+C-H-Y-I-N-G@users.noreply.github.com>
2021-04-01 03:09:22 -0700

    add code for removing \n in finishing position

--- group_project_data.R
+++ group_project_data.R
@@ -11,40 +11,33 @@ rm(list = ls(all.names = TRUE))# clear all
 gc()
 
 # libraries
-
 require(data.table)
 require(stargazer)
 require(ggplot2)
 require(PerformanceAnalytics)
 require(sqldf)
-require(dyplr)
-require(pastecs)
 
-#Load data from Github
-circuits <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/circuits.csv?token=ARGCUJVFLHKMHJWC6XIH7BDALPFH2")
-constructor_results <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_results.csv?token=ARGCUJVTEA7CUOKHQGVEMSTALPFUQ")
-constructor_standings <-fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_standings.csv?token=ARGCUJVL4UAEH6G4MG6FX5DALPGEE")
-constructors <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructors.csv?token=ARGCUJRM5BXALZ4LNR3HBZTALPGOW")
-driver_standings <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/driver_standings.csv?token=ARGCUJUEKGGVYUIJUPBIEF3ALPGRS")
-drivers <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/drivers.csv?token=ARGCUJVAAFN42GEWVT53LTDALPGU2")
+#load data
 lap_times <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/lap_times.csv?token=ARGCUJRBHFWPLGAR6JIU65TALPGZ4")
 pit_stops <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/pit_stops.csv?token=ARGCUJSONNTEXU74NYPGU3DALPG7Q")
 qualifying <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/qualifying.csv?token=ARGCUJX6ZEK3OU273Z62EETALPHFA")
+constructor_results <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_results.csv?token=ARGCUJVTEA7CUOKHQGVEMSTALPFUQ")
+constructor_standings <-fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/constructor_standings.csv?token=ARGCUJVL4UAEH6G4MG6FX5DALPGEE")
+driver_standings <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/driver_standings.csv?token=ARGCUJUEKGGVYUIJUPBIEF3ALPGRS")
 races <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/races.csv?token=ARGCUJTJIFZSYPYKCUAZJ2TALPHJG")
 results <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/results.csv?token=ARGCUJVOFVMB7TWWVUIK3DDALPHNC")
-seasons <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/seasons.csv?token=ARGCUJRUCR5MDWODLISRETLALPHYI")
 status <- fread("https://raw.githubusercontent.com/C-H-Y-I-N-G/BUS462/main/data/status.csv?token=ARGCUJSK3IMT54XZMKATKG3ALPH4M")
-
-head(results)
-
-dt <- merge(lap_times,drivers,by="driverId") #merge 2 data frame
-dt <- subset(dt, select = -c(url)) #drop url column
+# races table replace seasons table
+# driver standings table replace drivers table
+# constructor results and constructor standings replaces constructors table
 
 # organize races and results tables for merging
 races <- subset(races, select = -c(round,name,date,time,url))#drop columns
-results <- subset(results, select = -c(positionText,positionOrder))
-names(results)[names(results) == "position"] <- "race_position"
-names(results)[names(results) == "points"] <- "race_points"
+results <- subset(results, select = -c(positionText,positionOrder,time))
+names(results)[names(results) == "position"] <- "finishing_position"
+names(results)[names(results) == "points"] <- "finishing_points"
+names(results)[names(results) == "milliseconds"] <- "finishing_milliseconds"
+names(results)[names(results) == "laps"] <- "lap_finished"
 dt <- merge(races,results,by="raceId") #merge 2 data frame
 
 # merge status table
@@ -66,11 +59,36 @@ names(constructor_results)[names(constructor_results) == "points"] <- "construct
 dt <- merge(dt,constructor_standings, by=c("constructorId","raceId"))
 dt <- merge(dt,constructor_results, by=c("constructorId","raceId"))
 
+# organize qualifying tables for merging
+qualifying <- subset(qualifying, select = -c(number))
+names(qualifying)[names(qualifying) == "position"] <- "qualifying_position"
+dt <- merge(dt,qualifying,by=c("driverId","raceId","constructorId"))
+
+# organize pit stop tables for merging
+pit_stops <- subset(pit_stops, select = -c(duration,time))#duration has same number as milliseconds
+names(pit_stops)[names(pit_stops) == "stop"] <- "pit_stop"
+names(pit_stops)[names(pit_stops) == "lap"] <- "pit_stops_lap"
+names(pit_stops)[names(pit_stops) == "milliseconds"] <- "pit_stops_milliseconds"
+dt <- merge(dt,pit_stops, by=c("driverId","raceId"))
+
+# organize lap times tables for merging
+lap_times <- subset(lap_times, select = -c(time))
+names(lap_times)[names(lap_times) == "position"] <- "lap_times_position"
+names(lap_times)[names(lap_times) == "lap"] <- "lap_times_lap"
+names(lap_times)[names(lap_times) == "milliseconds"] <- "lap_times_milliseconds"
+dt <- merge(dt,lap_times, by=c("driverId","raceId"))
+
+# replace all the \N race position to 0
+dt$finishing_position <- as.integer(dt$finishing_position)
+dt$finishing_position[is.na(dt$finishing_position)] = 50 #can change to 21 
+
 dt[duplicated(dt)]#check duplication
 dt[!duplicated(dt)]#remove duplication
 
-head(dt)
+dt <- subset(dt, year > 2014)#shrink the table to year 2015-2020
 
+#look at data
+head(dt)
 View(dt)
 
 #create function that checks if any NAs are in a column
@@ -96,7 +114,6 @@ dt$rank <- as.integer(dt$rank)
 #convert fastest lap speed
 dt$fastestLapSpeed <- as.numeric(dt$fastestLapSpeed)
 dt$fastestLapSpeed[is.na(dt$fastestLapSpeed)] = 0 #for NAs set to 0
-
 
 #BEGINNING OF PRELIMINARY ANALYSIS 
 
